337ee81c9d1089905804fe097176802d6c3d2807
Mark Alzouibi <alzouibim@gmail.com>
2024-07-30 01:01:18 -0400

    Exercise 1-8 done

---
+++ chapter-1/exercise1-8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int c, nl, tabs, blanks;
+    nl = 0;
+    tabs = 0;
+    blanks = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            ++nl;
+        }
+        if (c == '\t') {
+            ++tabs;
+        }
+        if (c == ' ') {
+            ++blanks;
+        }
+    }
+
+    printf("\n");
+    printf("New lines: %d\n", nl);
+    printf("Tabs: %d\n", tabs);
+    printf("Blanks: %d\n", blanks);
+}
