403a6366631e6b4e6a085b77641d68ee68c1decc
Mark Alzouibi <alzouibim@gmail.com>
2024-08-09 23:02:07 -0400

    Rewrote detab to work with line buffer

--- chapter-1/exercise1-22.c
+++ chapter-1/exercise1-22.c
@@ -10,29 +10,29 @@
 
 #define FOLD_LENGTH 20
 #define MAXLINE 1000
+#define TABSTOP 8
 
 
 int mygetline(char s[], int lim);
-void fold(char s[], int len);
+void detab(char to[], char from[]);
 
 int main() {
     int len;
     char line[MAXLINE];
+    char detabbed[MAXLINE];
 
     while ((len = mygetline(line, MAXLINE)) > 0) {
-        if (len > FOLD_LENGTH) {
-            fold(line, len);
-        } else {
-            printf("%s", line);
-        }
+        detab(detabbed, line);
+        printf("%s", detabbed);
     }
+
     return 0;
 }
 
 int mygetline(char s[], int lim) {
     int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != ' '; ++i) {
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
         s[i] = c;
     }
 
@@ -46,25 +46,22 @@ int mygetline(char s[], int lim) {
     return i;
 }
 
-void fold(char s[], int len) {
-    int i, last_blank, column;
+void detab(char to[], char from[]) {
+    int i, j, pos, spaces;
 
-    last_blank = column = 0;
-    for (i = 0; i < len; ++i) {
-        if (s[i] == ' ' || s[i] == '\t') {
-            last_blank = i;
-        }
-        ++column;
-        if (column > FOLD_LENGTH) {
-            if (last_blank > 0) {
-                s[last_blank] = '\n';
-                column = i - last_blank;
-                last_blank = 0;
-            } else {
-                printf("\n");
-                column = 1;
+    pos = 0;
+    for (i = 0; from[i] != '\0'; ++i) {
+        if (from[i] == '\t') {
+            spaces = TABSTOP - (pos % TABSTOP);
+            for (j = 0; j < spaces; ++j) {
+                to[pos] = ' ';
+                ++pos;
             }
+        } else {
+            to[pos] = from[i];
+            ++pos;
         }
-        putchar(s[i]);
     }
+
+    to[pos] = '\0';
 }
